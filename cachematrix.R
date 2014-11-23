f0127c4b8f15caf9bdae64436bba95be2933c9be
Hajra Iftikhar <hajraiftikhar@gmail.com>
2014-11-22 20:54:58 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
+#The function below calls makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+i<-NULL #i will be the 'inverse' and its reset to NULL every 
+        #time makeCacheMatrix is called.
+get<-function(y) { #this function returns the value of the original vector
+    x<<-y
+    i<<-NULL
+}
+
+get<- function()x
+setinverse<-function(solve) i<<-inverse
+#this is called by cacheMatrix() during the first CacheMatrix() assignment and it will
+# store the value using superassignment
+getinverse<- function()i # this will return the value of CacheMatrix() on subsequent assignments
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
@@ -12,4 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+i<- x$getinverse()
+if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
 }
+data<- x$get
+i<- solve(data,...)
+x$setinverse(i)
+i}
